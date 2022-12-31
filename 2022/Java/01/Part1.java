b2130e0f4b3da411d1276cfa6f7254e19de5ea14
Phandal <bfeld.111@gmail.com>
2022-12-31 00:39:44 -0600

    started on 2022 d1 p1 in java

--- 2022/Java/01/Part1.java
+++ 2022/Java/01/Part1.java
@@ -5,9 +5,19 @@ import java.util.Scanner;
 public class Part1 {
   public static void main(String args[]) {
     try {
+
+      if (args.length < 2) {
+        System.err.println("Usage: part1 <filename>");
+        System.exit(1);
+      }
+
       File f = new File("test.txt");
       Scanner fileReader = new Scanner(f);
-      System.out.println(fileReader.nextLine());
+
+      while(fileReader.hasNext()) {
+        System.out.printf("%i\n", Integer.parseInt(fileReader.nextLine()));
+      }
+
       fileReader.close();
     } catch (FileNotFoundException e) {
       System.out.printf("Error: %s\n", e.getMessage());
