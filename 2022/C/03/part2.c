7d15adba8c518675f9447084f1a7bd9c396b95a9
Phandal <bfeld.111@gmail.com>
2023-01-02 15:14:07 -0600

    C Day 3 2022 part 2

--- 2022/C/03/part2.c
+++ 2022/C/03/part2.c
@@ -1,4 +1,3 @@
-// 7116 not the answer
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,101 +6,100 @@
 
 #define MAX_SIZE 4096
 #define TEST_INPUT_FILENAME "test_input.txt"
-#define TEST_INPUT_ANSWER 157
+#define TEST_INPUT_ANSWER 70
+
+enum { RUCK_SACK_1, RUCK_SACK_2, RUCK_SACK_3 };
 
 typedef struct {
-  char* item_list;
-  int num_items;
-} compartment_t;
+  char* rucksack_1;
+  char* rucksack_2;
+  char* rucksack_3;
+} group;
 
-typedef struct {
-  compartment_t* first_compartment;
-  compartment_t* second_compartment;
-  char item;
-  int priority;
-} rucksack_t;
+group* group_new() {
+  group* g = malloc(sizeof(group));
+  g->rucksack_1 = NULL;
+  g->rucksack_2 = NULL;
+  g->rucksack_3 = NULL;
 
-compartment_t * compartment_new() {
-  compartment_t* t = malloc(sizeof(compartment_t));
-  t->item_list = NULL;
-  return t;
+  return g;
 }
 
-void compartment_del(compartment_t* t) {
-  free(t->item_list);
-  free(t);
+void group_del(group* g) {
+  if (g->rucksack_1) { free(g->rucksack_1); }
+  if (g->rucksack_2) { free(g->rucksack_2); }
+  if (g->rucksack_3) { free(g->rucksack_3); }
+
+  free(g);
 }
 
-void compartment_fill(compartment_t* t, int start, int end, char* items) {
-  char* sub_string = malloc(sizeof(start - end + 1));
-  strncpy(sub_string, items + start, end - start);
-  t->item_list = sub_string;
-  t->num_items = strlen(sub_string);
-}
-
-void compartment_print(compartment_t* t) {
-  printf("%s : %i", t->item_list, t->num_items);
-}
-
-rucksack_t* rucksack_new() {
-  rucksack_t* r = malloc(sizeof(rucksack_t));
-  r->first_compartment = compartment_new();
-  r->second_compartment = compartment_new();
-  return r;
-}
-
-void rucksack_del(rucksack_t* r) {
-  compartment_del(r->first_compartment);
-  compartment_del(r->second_compartment);
-  free(r);
-}
-
-void rucksack_fill(rucksack_t* r, char* items) {
-  int num_items = strlen(items);
-  compartment_fill(r->first_compartment, 0, (num_items / 2), items);
-  compartment_fill(r->second_compartment, ((num_items / 2)), (num_items), items);
-}
-
-void rucksack_calculate_item(rucksack_t* r) {
-  int first_items[52] = {0};
-  for (int i = 0; i < r->first_compartment->num_items + 1; ++i) {
-    char first_item = r->first_compartment->item_list[i];
-    if (first_item > 91) {
-      first_items[first_item - 97 + 26]++;
-    } else {
-      first_items[first_item - 65]++;
-    }
+void group_add_rucksack(group* g, char* input, int rucksack_num) {
+  switch(rucksack_num) {
+  case RUCK_SACK_1:
+    g->rucksack_1 = malloc(strlen(input));
+    strcpy(g->rucksack_1, input);
+    break;
+  case RUCK_SACK_2:
+    g->rucksack_2 = malloc(strlen(input));
+    strcpy(g->rucksack_2, input);
+    break;
+  case RUCK_SACK_3:
+    g->rucksack_3 = malloc(strlen(input));
+    strcpy(g->rucksack_3, input);
+    break;
   }
+}
 
-  int second_items[52] = {0};
-  for (int i = 0; i < r->second_compartment->num_items + 1; ++i) {
-    char second_item = r->second_compartment->item_list[i];
-    if (second_item > 91) {
-      second_items[second_item - 97 + 26]++;
-    } else {
-      second_items[second_item - 65]++;
-    }
-  }
+char group_calculate_badge(group* g) {
+  size_t s1 = strlen(g->rucksack_1);
+  size_t s2 = strlen(g->rucksack_2);
+  size_t s3 = strlen(g->rucksack_3);
+  char* common_letters = NULL;
+  size_t count = 0;
+  char badge_letter;
+  
+  for(size_t i = 0; i < s1; i++) {
+    char l1 = g->rucksack_1[i];
 
-  for (int i = 0; i < 52; i++) {
-    // printf("i: %d, first: %d, second %d\n", i, first_items[i], second_items[i]);
-    if (first_items[i] > 0 && second_items[i] > 0) {
-      if (i < 26) {
-        r->item =  i + 65;
-        r->priority = i + 1 + 26;
-      } else {
-        r->item = i + 97 - 26;
-        r->priority = i + 1 - 26;
+    for (size_t ii = 0; ii < s2; ii++) {
+      char l2 = g->rucksack_2[ii];
+      
+      if (l1 == l2) {
+        ++count;
+        common_letters = realloc(common_letters, sizeof(char) * count);
+        common_letters[count - 1] = l1;
       }
     }
   }
+
+  for (size_t i = 0; i < s3; i++) {
+    char l3 = g->rucksack_3[i];
+
+    for (size_t ii = 0; ii < count; ii++) {
+      char cl = common_letters[ii];
+
+      if (cl == l3) {
+        badge_letter = cl;
+      }
+    }
+  }
+
+  free(common_letters);
+  return badge_letter;
 }
 
-void rucksack_print(rucksack_t* r) {
-  compartment_print(r->first_compartment);
-  printf(" : ");
-  compartment_print(r->second_compartment);
-  printf(" : %c : %d\n", r->item, r->priority);
+void group_reset(group* g) {
+  g->rucksack_1 = NULL;
+  g->rucksack_2 = NULL;
+  g->rucksack_3 = NULL;
+}
+
+long calculate_priority(char badge_letter) {
+  if (badge_letter > 92) {
+    return badge_letter - 96;
+  } else {
+    return badge_letter - 64 + 26;
+  }
 }
 
 int main(int argc, char** argv) {
@@ -119,17 +117,35 @@ int main(int argc, char** argv) {
   
   char* input = malloc(MAX_SIZE);
   long result = 0;
-  rucksack_t* r = rucksack_new();
+  int cont = 1;
+  group* g = group_new();
   
-  while(fgets(input, MAX_SIZE, fd)) {
+  while(cont) {
     // This is where you start writeing code for each challenge
-    input[strlen(input) - 1] = '\0';
 
-    rucksack_fill(r, input);
-    rucksack_calculate_item(r);
-    // rucksack_print(r);
-    // printf("first: %s:\tsecond: %s\titem: %c\tpriority: %d\tresult: %li\n", r->first_compartment->item_list, r->second_compartment->item_list, r->item, r->priority, result);
-    result += r->priority;
+    for (int i = 0; i < 3; i++) {
+      if (!(fgets(input, MAX_SIZE, fd))) {
+        if (i > 0) {
+          fprintf(stderr, "Error: EOF reached!\n");
+          group_del(g);
+          free(input);
+          fclose(fd);
+          return 1;
+        }
+        else {
+          cont = 0;
+          break;
+        }
+      }
+      input[strlen(input) - 1] = '\0';
+      group_add_rucksack(g, input, i);
+    }
+
+    if (cont) {
+      char badge = group_calculate_badge(g);
+      result += calculate_priority(badge);
+    }
+    group_reset(g);
   }
 
   printf("Result: %li\n", result);
@@ -137,7 +153,7 @@ int main(int argc, char** argv) {
   // This is where you stop write code
 
   // Cleanup
-  rucksack_del(r);
+  group_del(g);
   free(input);
   fclose(fd);
 
