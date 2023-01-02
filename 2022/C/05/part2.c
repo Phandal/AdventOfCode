bce069bf5af37294c22dd566942440211a312cd2
Phandal <bfeld.111@gmail.com>
2023-01-02 17:36:06 -0600

    Start of Day 05 2022 in C

---
+++ 2022/C/05/part2.c
@@ -0,0 +1,48 @@
+// 7116 not the answer
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdbool.h>
+
+#include "util.h"
+
+#define MAX_SIZE 4096
+#define TEST_INPUT_FILENAME "test_input.txt"
+#define TEST_INPUT_ANSWER 2
+
+int main(int argc, char** argv) {
+  // File Reading Operations
+  errno = 0;
+  FILE* fd = read_file(argc, argv);
+  if (!fd) {
+    return -1;
+  }
+  
+  char* input = malloc(MAX_SIZE);
+  long result = 0;
+  
+  while(fgets(input, MAX_SIZE, fd)) {
+    input[strlen(input) - 1] = '\0';
+
+    // This is where you start writing code for each challenge
+    
+  }
+
+  printf("Result: %li\n", result);
+
+  // This is where you stop write code
+
+  // Put cleanup code here
+  free(input);
+  fclose(fd);
+
+  // Test Case Here
+  if (strcmp(argv[1], TEST_INPUT_FILENAME) == 0 ) {
+    assert(result == TEST_INPUT_ANSWER);
+    puts("Test Passed!");
+  }
+
+  return 0;
+}
---
+++ 2022/C/05/util.h
@@ -0,0 +1,8 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdio.h>
+
+FILE* read_file(int argc, char** argv);
+
+#endif // UTIL_H
