f35eca9ce9d66da09694b29b72f67ea956a0e13e
Phandal <bfeld.111@gmail.com>
2023-01-02 23:52:52 -0600

    C Day 5 2022

--- 2022/C/05/util.c
+++ 2022/C/05/util.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "util.h"
 
 FILE* read_file(int argc, char** argv) {
   if (argc < 2) {
@@ -13,4 +16,71 @@ FILE* read_file(int argc, char** argv) {
   }
 
   return fd;
+}
+
+node_t* node_new(char data) {
+  node_t* n = malloc(sizeof(node_t));
+  n->data = data;
+  n->next = NULL;
+  return n;
+}
+
+void node_del(node_t* n) {
+  free(n);
+}
+
+dynamic_stack_t* dynamic_stack_init() {
+  dynamic_stack_t* d = malloc(sizeof(dynamic_stack_t));
+  d->count = 0;
+  d->head = NULL;
+  return d;
+}
+
+void dynamic_stack_push(dynamic_stack_t* d, char data) {
+  d->count++;
+  node_t* n = node_new(data);
+  n->next = d->head;
+  d->head = n;
+}
+
+void dynamic_stack_append(dynamic_stack_t* d, char data) {
+  node_t* cur = d->head;
+
+  if (cur == NULL) {
+    dynamic_stack_push(d, data);
+    return;
+  }
+
+  d->count++;
+  node_t* n = node_new(data);
+  while(cur->next != NULL) {
+    cur = cur->next;
+  }
+
+  cur->next = n;
+}
+
+char dynamic_stack_pop(dynamic_stack_t* d) {
+  d->count--;
+  char data = d->head->data;
+  node_t* temp = d->head;
+  d->head = d->head->next;
+  node_del(temp);
+  return data;
+}
+
+void dynamic_stack_print(dynamic_stack_t* d) {
+  node_t* cur = d->head;
+  
+  while(cur != NULL) {
+    printf("%c", cur->data);
+    cur = cur->next;
+  }
+}
+
+void dynamic_stack_del(dynamic_stack_t* d) {
+  for (size_t i = 0; i < d->count; i++) {
+    dynamic_stack_pop(d);
+  }
+  free(d);
 }
--- 2022/C/05/util.h
+++ 2022/C/05/util.h
@@ -5,4 +5,24 @@
 
 FILE* read_file(int argc, char** argv);
 
+typedef struct node {
+  char data;
+  struct node* next;
+} node_t;
+
+typedef struct {
+  size_t count;
+  node_t* head;
+} dynamic_stack_t;
+
+node_t* node_new(char data);
+void node_del(node_t* n);
+
+dynamic_stack_t* dynamic_stack_init();
+void dynamic_stack_push(dynamic_stack_t* d, char data);
+void dynamic_stack_append(dynamic_stack_t* d, char data);
+char dynamic_stack_pop(dynamic_stack_t* d);
+void dynamic_stack_print(dynamic_stack_t* d);
+void dynamic_stack_del(dynamic_stack_t* d);
+
 #endif // UTIL_H
