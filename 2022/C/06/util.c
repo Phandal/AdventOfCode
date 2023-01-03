da08aea1ae97a2d61d850e472852b886417152bd
Phandal <bfeld.111@gmail.com>
2023-01-03 00:08:09 -0600

    started on C day 6 2022

---
+++ 2022/C/06/util.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "util.h"
+
+FILE* read_file(int argc, char** argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage: part1 <filename>\n");
+    return NULL;
+  }
+  
+  FILE* fd = fopen(argv[1], "r");
+  if (fd == NULL) {
+    perror("Error");
+    return NULL;
+  }
+
+  return fd;
+}
